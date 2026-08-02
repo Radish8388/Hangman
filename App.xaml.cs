65fb84b9cdfaa93b20b24e09b11e8ad73241032a
Paul B Colburn <paulcolburn@outlook.com>
2026-08-01 20:05:47 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+#if !DEBUG
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+#endif
+        }
+    }
+
+}
---
+++ ChooseLanguage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Hangman
+{
+    /// <summary>
+    /// Interaction logic for ChooseLanguage.xaml
+    /// </summary>
+    public partial class ChooseLanguage : Window
+    {
+        public int LanguageId { get; set; }
+        public ChooseLanguage()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (AE.IsChecked == true) LanguageId = 0;
+            if (BE.IsChecked == true) LanguageId = 1;
+            DialogResult = true;
+        }
+    }
+}
